1f5c3e5ed621ab247967a67d4dbbc25d580c261d
Vlad <m0glan@proton.me>
2023-07-03 11:05:03 +0200

    Made chatty-container name pool more random.

--- app/components/block-content/chatty-container.js
+++ app/components/block-content/chatty-container.js
@@ -4,7 +4,7 @@ import { tracked } from '@glimmer/tracking';
 export default class ChattyContainerComponent extends Component {
     @tracked greeting;
 
-    names = [ 'Jan', 'Dave', 'Vlad', 'Olesia', 'Serhii' ];
+    names = [ 'Bob', 'Alice', 'Jeremy', 'John', 'Mary' ];
 
     greet = () => {
         const name = this.names[Math.floor(Math.random()*this.names.length)];
